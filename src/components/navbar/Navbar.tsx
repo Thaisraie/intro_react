ba9f45bd51e2415df7b9f5baf5c83a71b15b940f
raiedev <thaisdivino21@gmail.com>
2024-05-27 15:21:25 -0300

    Criando componente Postagem.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-﻿﻿import { Link, useNavigate } from "react-router-dom"
+﻿﻿﻿import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext"
 import { useContext } from "react"
 
@@ -28,22 +28,24 @@ function Navbar() {
      * o logout foi efetuado com sucesso e redireciona o usuário para a 
      * Página de Login, através da constante navigate. 
      */
-    function logout(){
-        handleLogout() // zera o token. 
+    function logout() {
+        handleLogout()
         alert('O usuário foi desconectado com sucesso!')
-        navigate('/login')
+        navigate('/')
     }
+
+
 
     return (
         <>
             <div className='w-full flex justify-center py-4
             			   bg-indigo-900 text-white'>
-            
+
                 <div className="container flex justify-between text-lg">
                     <Link to="/home" className="text-2xl font-bold">Blog Pessoal</Link>
 
                     <div className='flex gap-4'>
-                        Postagens
+                        <Link to='/postagens' className='hover:underline'>Postagens</Link>
                         <Link to='/temas' className='hover:underline'>Temas</Link>
                         <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
                         Perfil
